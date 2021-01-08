de8eb50d4ba96ffa49da423e0a82fd2aa6aac242
jonathanccardoso <jonathan99moura@hotmail.com>
2021-01-08 17:18:19 -0300

    Fixme: resolve page account with form

--- core/forms.py
+++ core/forms.py
@@ -13,9 +13,9 @@ from allauth.account.forms import SignupForm
 class AllauthCompatLoginForm(LoginForm):
 	def user_credentials(self):
 		credentials = super(AllauthCompatLoginForm, self).user_credentials()
-		print("credentials", credentials)
-		credentials['login'] = credentials.get('siape')
-		# credentials['login'] = credentials.get('email') or credentials.get('username')
+		# print("credentials", credentials)
+		# credentials['login'] = credentials.get('siape')
+		credentials['login'] = credentials.get('email') or credentials.get('username')
 		return credentials
 
 
